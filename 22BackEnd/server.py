6115145a30fb16e3f2244f9eb77aae1b736404d3
fazaanhassan <zcabfha@ucl.ac.uk>
2018-11-10 01:34:01 +0000

    Working on sending answers

--- 22BackEnd/server.py
+++ 22BackEnd/server.py
@@ -1,8 +1,7 @@
-from pymongo import MongoClient
 
+from pymongo import MongoClient
 from stop_words import get_stop_words
 import nltk
-
 import pprint
 
 client = MongoClient()
@@ -12,32 +11,51 @@ db = client['MYDB']
 collection = db['customer']
 customer = db.customer
 
-pprint.pprint(customer.find_one())
+# pprint.pprint(customer.find_one())
+
 qndict = customer.find_one()
 
 ### Parser Function ###
+
 stop_words = set(get_stop_words('english'))
-def myKeywordParser(myString):
-	word_tokens = nltk.word_tokenize(myString)
-  filtered_Sentence = []
-  for w in word_tokens:
-		if w not in stop_words:
-			filtered_sentence.append(w)
-	return filtered_sentence
+
+
+def myKeyWordParser(myString):
+    word_tokens = nltk.tokenize.RegexpTokenizer('\\b\\w*[a-zA-Z]\\w+\\b').tokenize(myString)
+    filtered_sentence = []
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+    return filtered_sentence
+
+
+### End of Parser Function ####
 
 ####### Parsing of user Question #######
-input_1 = "do you share data"
-parsed_input = myKeyWorldParser(input_1)
+
+input_1 = 'do you share data'.lower()
+parsed_input = myKeyWordParser(input_1)
+
+
 ####### End of Parsing  user question ########
 
-question_filter = []
+def checkOccurrences(parsed_input):
+    possible_questions = []
+    for qn in qndict:
+        input_ = qndict[qn]
+        if qn == '_id':
+            continue
 
-for qn in qndict:
-    input_ = qndict[qn]
+        question_parsed = myKeyWordParser(input_['question'].lower())
+        counter = 0
+        for keyword in parsed_input:
+            if keyword in question_parsed:
+                counter += 1
+                print(counter)
+            # if keyword not in input_['keywords']:
+            #     input_['keywords'].append(keyword)
+            possible_questions.append((input_['question'], counter, input_['links']))
+    print(possible_questions)
 
-    if qn == '_id':
-        continue
-    
-    question_filter.append(myKeywordParser(input_))
 
-print(question_filter)
+checkOccurrences(parsed_input)
---
+++ 22BackEnd/test.py
@@ -0,0 +1,2 @@
+a = "ABC".lower()
+print(a)
